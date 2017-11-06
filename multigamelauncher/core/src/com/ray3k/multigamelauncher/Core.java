defb66ec925517d61798a84bb48d3a111589fc1d
Raymond Buckley <raymond.ray3k@gmail.com>
2017-11-06 09:53:19 -0800

    Added user interface.

--- multigamelauncher/core/src/com/ray3k/multigamelauncher/Core.java
+++ multigamelauncher/core/src/com/ray3k/multigamelauncher/Core.java
@@ -3,31 +3,67 @@ package com.ray3k.multigamelauncher;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Core extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private Skin skin;
+    private Stage stage;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        skin = new Skin(Gdx.files.internal("ui/metal-ui.json"));
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        
+        Table root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+        
+        root.defaults().space(20.0f);
+        TextButton textButton = new TextButton("Test Game 1", skin);
+        root.add(textButton);
+        
+        textButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+                
+            }
+        });
+        
+        textButton = new TextButton("Test Game 2", skin);
+        root.add(textButton);
+        
+        textButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+                
+            }
+        });
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(239.0f / 255.0f, 239.0f / 255.0f, 239.0f / 255.0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        skin.dispose();
+        stage.dispose();
+    }
 }
